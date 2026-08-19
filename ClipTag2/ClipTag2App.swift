ca155095d3f892949d00bc30454bf20e4a7a3f25
Lehshak Teelockchand <lehshak.teelockchand@mail.utoronto.ca>
2026-08-19 12:35:54 -0400

    Initial Commit

---
+++ ClipTag2/ClipTag2App.swift
@@ -0,0 +1,17 @@
+//
+//  ClipTag2App.swift
+//  ClipTag2
+//
+//  Created by Lehshak Tee on 2026-08-19.
+//
+
+import SwiftUI
+
+@main
+struct ClipTag2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipTag2Tests/ClipTag2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipTag2Tests.swift
+//  ClipTag2Tests
+//
+//  Created by Lehshak Tee on 2026-08-19.
+//
+
+import Testing
+@testable import ClipTag2
+
+struct ClipTag2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
